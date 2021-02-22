854cfbccd1e197a1e088af9414884d9d146dc0fc
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-22 11:14:22 -0500

    reading and writing files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,3 +1,11 @@
 const fs = require('fs')
-const hello = "Hello World"
-console.log(hello)
+ // reading and writing files
+
+ const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+ console.log(textIn)
+
+
+ const textOut = `This is what we know abut the Avocado: ${textIn}.\n Created on ${Date.now()}`
+ fs.writeFileSync('./txt/output.txt', textOut)
+
+ console.log(textOut)
