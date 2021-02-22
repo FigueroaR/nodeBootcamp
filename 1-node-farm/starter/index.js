93d54475108cb967ab595bd48a4ef634de995862
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-22 12:18:33 -0500

    async fs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,11 +1,19 @@
 const fs = require('fs')
  // reading and writing files
 
- const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
- console.log(textIn)
+
+// Blocking Synchonus way
+//  const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+//  console.log(textIn)
 
 
- const textOut = `This is what we know abut the Avocado: ${textIn}.\n Created on ${Date.now()}`
- fs.writeFileSync('./txt/output.txt', textOut)
+//  const textOut = `This is what we know abut the Avocado: ${textIn}.\n Created on ${Date.now()}`
+//  fs.writeFileSync('./txt/output.txt', textOut)
 
- console.log(textOut)
+//  console.log(textOut)
+
+
+ //Async fs
+ fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    console.log(data)
+ } )
